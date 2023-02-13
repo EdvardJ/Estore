9552c107371087095cc4fb0af0cce4aee12b8a1b
EdvardJ <eetu.jacobson@gmail.com>
2023-02-13 10:41:56 +0200

    Fix image urls (https)

--- estore_django/product/models.py
+++ estore_django/product/models.py
@@ -39,18 +39,18 @@ class Product(models.Model):
 
     def get_image(self):
         if self.image:
-            return 'http://myestore.info' + self.image.url
+            return 'https://myestore.info' + self.image.url
         return ''
     
     def get_thumbnail(self):
         if self.thumbnail:
-            return 'http://myestore.info' + self.thumbnail.url
+            return 'https://myestore.info' + self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
 
-                return 'http://myestore.info' + self.thumbnail.url
+                return 'https://myestore.info' + self.thumbnail.url
             else:
                 return ''
 
